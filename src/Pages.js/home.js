d669a9c3e8ec53f34dc7e9b557b63214b03df3fd
EliteProgrammerLab <craftsworkshop123@gmail.com>
2021-04-01 04:12:03 +0100

    filter-component

--- src/Pages.js/home.js
+++ src/Pages.js/home.js
@@ -10,11 +10,56 @@ function Home() {
 		size: "",
 		sort: "",
 	});
+
+	const sortProduct = (e) => {
+		const sort = e.target.value;
+		setProduct({
+			sort,
+			products: product.products
+				.slice()
+				.sort((a, b) =>
+					sort === "lowest"
+						? a.price > b.price
+							? 1
+							: -1
+						: sort === "hightest"
+						? a.price < b.price
+							? 1
+							: -1
+						: a._id < b._id
+						? 1
+						: -1
+				),
+		});
+		console.log("sortProduct", sort, product);
+	};
+	const filterProducts = (e) => {
+		const size = e.target.value;
+		size === ""
+			? setProduct({
+					sort: product.sort,
+					size,
+					products: data.products,
+			  })
+			: setProduct({
+					sort: product.sort,
+					size,
+					products: data.products.filter(
+						(product) => product.availableSizes.indexOf(size) >= 0
+					),
+			  });
+	};
 	return (
 		<div>
 			<div className='content'>
 				<div className='main'>
-					<Filter />
+					<Filter
+						count={product.products.length}
+						size={product.size}
+						sort={product.sort}
+						filterHandler={filterProducts}
+						sortHandler={sortProduct}
+					/>
 					<Products data={product.products} />
 				</div>
 			</div>
