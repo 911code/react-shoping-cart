17f24aef0b62b6249082289e91db64bf4f53c8c4
EliteProgrammerLab <craftsworkshop123@gmail.com>
2021-04-01 01:48:29 +0100

    creat product component

---
+++ src/components/Products.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Products({ data }) {
+	const formatCurrency = (num) => {
+		return ` $ ${Number(num.toFixed(2)).toLocaleString()} `;
+	};
+	return (
+		<div>
+			<ul className='products'>
+				{data.map((item) => {
+					const { _id, image, title, price } = item;
+					return (
+						<li key={_id}>
+							<div className='product'>
+								<a href={`# ${_id}`}>
+									<img src={image} alt={title} />
+									<p>{title}</p>
+								</a>
+								<div className='product-price'>{formatCurrency(price)}</div>
+								<button className='button primary'>Add To Chart</button>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
+
+export default Products;
